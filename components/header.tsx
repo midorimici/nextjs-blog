b15e493b4ef1be610f523780433ed109b345cbb4
mici <60635066+midorimici@users.noreply.github.com>
2021-07-14 23:34:26 +0900

    💄 #1 add brand mi to header

--- components/header.tsx
+++ components/header.tsx
@@ -2,29 +2,33 @@ import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTags, faUser } from '@fortawesome/free-solid-svg-icons'
 
+import BrandMi from './brand-mi';
+
+const buttonStyle = "flex items-center gap-2 hover:text-pink-400 transition-colors duration-300"
+const iconStyle = "w-6"
+
 const Header = () => {
   return (
     <header className="py-8">
       <div className="flex justify-between items-center">
         <Link href="/">
-          <a>
+          <a className="flex items-center">
+            <BrandMi />
             <span className="font-bold text-5xl">
-              みどりみちのブログ
+              どりみちのブログ
             </span>
           </a>
         </Link>
         <nav className="flex items-center gap-8">
           <Link href="/tags">
-            <a className='hover:text-pink-400 transition-colors duration-300'>
-              <FontAwesomeIcon icon={faTags} />
-              &nbsp;
+            <a className={buttonStyle}>
+              <FontAwesomeIcon icon={faTags} className={iconStyle} />
               Tags
             </a>
           </Link>
           <Link href="/about">
-            <a className='hover:text-pink-400 transition-colors duration-300'>
-              <FontAwesomeIcon icon={faUser} />
-              &nbsp;
+            <a className={buttonStyle}>
+              <FontAwesomeIcon icon={faUser} className={iconStyle} />
               About
             </a>
           </Link>
