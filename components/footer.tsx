3630206a87398015b17b15faef07fb111948c991
mici <60635066+midorimici@users.noreply.github.com>
2021-07-16 14:12:25 +0900

    💄 #1 responsive footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 const Footer = () => {
   return (
     <footer>
-      <div className="py-8 flex justify-center items-center gap-2">
-        {`© 2020–${(new Date()).getFullYear()}`}
+      <div className="py-8 flex justify-center items-center gap-2 text-sm sm:text-base">
+        {`© 2020–${(new Date()).getFullYear() - 2000}`}
         <FontAwesomeIcon icon={faStar} className="w-5 inline-block animate-rotate" />
         みどりみちのブログ
       </div>
