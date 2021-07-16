0641f5c52e059cdb6360e07dbd4f3d2da03d7947
mici <60635066+midorimici@users.noreply.github.com>
2021-07-16 14:58:37 +0900

    💄 #1 align posts in top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
+import Stories from '../components/stories'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
@@ -13,8 +11,6 @@ type Props = {
 }
 
 const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -22,18 +18,7 @@ const Index = ({ allPosts }: Props) => {
           <title>{SITE_NAME}</title>
         </Head>
         <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <Stories posts={allPosts} />}
         </Container>
       </Layout>
     </>
