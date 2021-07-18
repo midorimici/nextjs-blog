ee97c13cd8b69b4e1eb0288debef6d6e2bcfcf5f
mici <60635066+midorimici@users.noreply.github.com>
2021-07-18 22:27:08 +0900

    💄 #2 update code element styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code|IBM+Plex+Serif:400,400i,500,700,700i|Source+Code+Pro:400,400i,700,700i&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
