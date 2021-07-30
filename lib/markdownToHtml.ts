22c9b1aa9367dd482587069ee11b904da647e9fc
mici <60635066+midorimici@users.noreply.github.com>
2021-07-30 22:56:18 +0900

    🐛 #2 render tltp as normal string in summary

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,6 +2,9 @@ import remark from 'remark'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString().replace(/<pstlk label="(.+?)" to=".+?" \/>/g, '$1')
+  const parseMarkdown = markdown
+    .replace(/<pstlk label="(.+?)" to=".+?" \/>/g, '$1')
+    .replace(/<tltp label="(.+)">(?:(?:.|\s)+)<\/tltp>/, '$1')
+  const result = await remark().use(html).process(parseMarkdown)
+  return result.toString()
 }
