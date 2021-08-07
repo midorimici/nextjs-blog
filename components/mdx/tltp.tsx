dd29053e96af47e7d1a3c0558fb82366e97544f8
mici <60635066+midorimici@users.noreply.github.com>
2021-08-07 21:33:50 +0900

    📱 #2 responsive for tltp

--- components/mdx/tltp.tsx
+++ components/mdx/tltp.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { useState, useEffect, useRef, ReactElement } from 'react'
 import { useParsedMarkdown } from '../useParsedMarkdown'
 
 export type TooltipProps = {
@@ -10,12 +10,29 @@ const Tooltip = ({ label, children }: TooltipProps) => {
   const content = useParsedMarkdown(children)
   const parsedLabel = useParsedMarkdown(label)
 
+  const [tooltipOverflowStyle, setTooltipOverflowStyle] = useState('');
+  const tooltipRef = useRef<HTMLSpanElement>(null)
+
+  const alignTooltip = () => {
+    if (tooltipRef.current
+      && tooltipRef.current.getBoundingClientRect().right + 16 > document.documentElement.clientWidth
+    ) {
+      setTooltipOverflowStyle('right-0')
+    }
+  }
+
+  useEffect(() => { alignTooltip() }, [tooltipRef.current])
+
   return (
-    <span className="tooltip-container relative text-pink-400" onTouchStart={() => {}}>
+    <span
+      className="tooltip-container relative text-pink-400"
+      onTouchStart={() => {}}
+    >
       <span
+        ref={tooltipRef}
         className={`
           tooltip
-          absolute bottom-full mb-4 p-2
+          absolute bottom-full ${tooltipOverflowStyle} mb-4 p-2
           opacity-0 bg-pink-100 rounded text-sm
           transition duration-300
         `}
