43e305fb829748a0b91ad2eb5a5d36201a20c75c
mici <60635066+midorimici@users.noreply.github.com>
2021-08-19 23:13:51 +0900

    🐛 fix image url

--- components/stories.tsx
+++ components/stories.tsx
@@ -8,21 +8,19 @@ type Props = {
 const Stories = ({ posts }: Props) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-8 gap-y-8 mb-4">
-      {posts.filter((post) => post.published).map((post) => (
-        <PostPreview
+      {posts.filter((post) => post.published).map((post) => {
+        const coverImage = post.assets.find(asset => asset.fields.file.fileName === '_index.jpg')
+        return <PostPreview
           key={post.slug}
           slug={post.slug}
           title={post.title}
           date={post.date}
           lastmod={post.lastmod}
           topics={post.topics.map(topic => topic.fields)}
-          coverImageUrl={`
-            https:${post.assets
-              .find(asset => asset.fields.file.fileName === '_index.jpg')?.fields.file.url ?? ''}
-          `}
+          coverImageUrl={`https:${coverImage?.fields.file.url ?? ''}`}
           summary={(post.summary ?? post.content.replace(/([\s\S]+)\n<!--more-->[\s\S]+/, '$1')) + '…'}
         />
-      ))}
+      })}
     </div>
   )
 }
