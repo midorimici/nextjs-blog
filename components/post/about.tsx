1fc84152be0cb547b486af6b0388776c4ce68dc1
mici <60635066+midorimici@users.noreply.github.com>
2021-08-21 18:31:50 +0900

    ♻️ use remark instead of mdx remote

--- components/post/about.tsx
+++ components/post/about.tsx
@@ -1,27 +1,28 @@
-import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
-import { MDXRemote } from 'next-mdx-remote'
+// import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
+// import { MDXRemote } from 'next-mdx-remote'
 
-import Heading from 'components/mdx/heading'
+// import Heading from 'components/mdx/heading'
 import markdownStyles from './markdown-styles.module.css'
 
 type Props = {
-  source: MDXRemoteSerializeResult<Record<string, unknown>>
+  content: string // MDXRemoteSerializeResult<Record<string, unknown>>
 }
 
-const AboutPost = ({ source }: Props) => {
+const AboutPost = ({ content }: Props) => {
   /* eslint-disable react/display-name */
-  const components = {
-    a: ({ href, children }: { href: string, children: string }) => (
-      <a href={href} target="_blank" rel="noopener noreferrer">{children}</a>
-    ),
-    h2: ({ children }: { children: any }) => <Heading type={2} content={children} />,
-    h3: ({ children }: { children: any }) => <Heading type={3} content={children} />,
-  }
+  // const components = {
+  //   a: ({ href, children }: { href: string, children: string }) => (
+  //     <a href={href} target="_blank" rel="noopener noreferrer">{children}</a>
+  //   ),
+  //   h2: ({ children }: { children: any }) => <Heading type={2} content={children} />,
+  //   h3: ({ children }: { children: any }) => <Heading type={3} content={children} />,
+  // }
 
   return (
-    <div className={`${markdownStyles.markdown} max-w-2xl mx-auto`}>
-      <MDXRemote {...source} components={components} />
-    </div>
+    <div
+      className={`${markdownStyles.markdown} max-w-2xl mx-auto`}
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
   )
 }
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,5 @@
-import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
+// import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
+// import { serialize } from 'next-mdx-remote/serialize'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -7,14 +7,15 @@ import Layout from 'components/layout'
 import AboutPost from 'components/post/about'
 import { getAboutPost } from 'lib/api'
 import { SITE_NAME } from 'lib/constants'
+import markdownToHtml from 'lib/markdownToHtml'
 
 type Props = {
   postTitle: string
   profileUrl: string
-  source: MDXRemoteSerializeResult<Record<string, unknown>>
+  content: string // MDXRemoteSerializeResult<Record<string, unknown>>
 }
 
-const About = ({ postTitle, profileUrl, source }: Props) => {
+const About = ({ postTitle, profileUrl, content }: Props) => {
   return (
     <>
       <Head>
@@ -39,7 +40,7 @@ const About = ({ postTitle, profileUrl, source }: Props) => {
                 priority
               />
             </div>
-            <AboutPost source={source} />
+            <AboutPost content={content} />
           </div>
         </article>
       </Layout>
@@ -51,13 +52,14 @@ export default About
 
 export async function getStaticProps() {
   const post = await getAboutPost()
-  const content = await serialize(post.content || '')
+  const content = await markdownToHtml(post.content ?? '')
+  // const content = await serialize(post.content || '')
 
   return {
     props: {
       postTitle: post.title,
       profileUrl: post.profileUrl,
-      source: content,
+      content,
     },
   }
 }
