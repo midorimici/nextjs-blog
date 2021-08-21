2a6afab7e21952a5893afbb663141e3b04d618e7
mici <60635066+midorimici@users.noreply.github.com>
2021-08-21 19:02:42 +0900

    ⚡️ dynamic import topic tip on tags page

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 
 import Layout from 'components/layout'
-import TopicTip from 'components/topic-tip'
 import type { ContentfulTopicFields } from 'types/api'
 import { SITE_NAME, TAGS_PAGE_TITLE } from 'lib/constants'
 import { getPostNumbersByTopics } from 'lib/api'
+
+const TopicTip = dynamic(() => import('components/topic-tip'))
 
 type Props = {
   topicNumberMap: Record<string, { count: number, topic: ContentfulTopicFields }>
@@ -12,29 +14,31 @@ type Props = {
 
 const Tags = ({ topicNumberMap }: Props) => {
   return (
-    <Layout>
-      <article className="mb-8">
-        <Head>
-          <title>
-            {TAGS_PAGE_TITLE} | {SITE_NAME}
-          </title>
-        </Head>
-        <h1
-          className={`
-            max-w-2xl mx-auto mb-12
-            text-2xl sm:text-4xl text-left font-bold tracking-wide break-all
-          `}
-        >{TAGS_PAGE_TITLE}</h1>
-        <div className="max-w-2xl mx-auto flex flex-wrap gap-4">
-          {Object.entries(topicNumberMap).map((
-              [topicId, topic]: [string, { count: number, topic: ContentfulTopicFields }]
-            ) => (
-              <TopicTip key={topicId} topic={topic.topic} number={topic.count} />
-            )
-          )}
-        </div>
-      </article>
-    </Layout>
+    <>
+      <Head>
+        <title>
+          {TAGS_PAGE_TITLE} | {SITE_NAME}
+        </title>
+      </Head>
+      <Layout>
+        <article className="mb-8">
+          <h1
+            className={`
+              max-w-2xl mx-auto mb-12
+              text-2xl sm:text-4xl text-left font-bold tracking-wide break-all
+            `}
+          >{TAGS_PAGE_TITLE}</h1>
+          <div className="max-w-2xl mx-auto flex flex-wrap gap-4">
+            {Object.entries(topicNumberMap).map((
+                [topicId, topic]: [string, { count: number, topic: ContentfulTopicFields }]
+              ) => (
+                <TopicTip key={topicId} topic={topic.topic} number={topic.count} />
+              )
+            )}
+          </div>
+        </article>
+      </Layout>
+    </>
   )
 }
 
